ec462463e582274ff0569db1ae2346dc33af1e3b
aadesh73132 <145355002+aadesh73132@users.noreply.github.com>
2023-11-04 20:30:24 +0530

    Add files via upload

---
+++ day41.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    
+    printf("enter the value of a and b you want the sum of\n");
+    scanf("%f%f",&a,&b);
+    c=a+b;
+    printf("c =%f",c);
+    
+    return 0;
+}
